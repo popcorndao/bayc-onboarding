1ad462676930774ce13a156c2540cf9abb3f7ea9
RedVeil <leon.niesler@gmail.com>
2021-11-15 09:28:30 +0100

    not available on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -153,191 +153,198 @@ export default function Index(): JSX.Element {
     <div className="w-full h-screen bg-primary overflow-hidden">
       <Navbar />
       <div className="z-10 relative flex justify-center mt-8 xl:mt-16 2xl:mt-24">
-        <div className="flex flex-col w-1/3 mt-4">
+        <div className="flex flex-col w-full md:w-2/3 lg:w-1/3 mt-4">
           <img
             src="/images/hero.png"
             alt="hero"
             className="w-9/12 xl:w-full mx-auto"
           />
-          <p className="text-lg xl:text-xl 2xl:text-3xl mt-8 2xl:mt-12 text-gray-900 font-light text-center">
-            This airdrop sends 100 $POP to your wallet and 100 $POP to the
-            charities you select. Your airdropped tokens are locked until $POP
-            staking 2022). Simply verify BAYC charity allocations, and await
-            your airdrop. In meantime, join us in{" "}
-            <a
-              className="font-normal cursor-pointer"
-              href="https://discord.gg/RN4VGqPDwX"
-              target="_blank"
-            >
-              Discord
-            </a>{" "}
-            and follow us on{" "}
-            <a
-              className="font-normal cursor-pointer"
-              href="https://twitter.com/popcorn_DAO"
-              target="_blank"
-            >
-              Twitter
-            </a>
-          </p>
-          {PHASE === Phase.SignUp ? (
-            <>
-              {step === Step.End ? (
-                <>
-                  {submitSuccess ? (
-                    <div>
-                      <p className="text-2xl 2xl:text-4xl text-center mt-8">
-                        Congratulations!
-                      </p>
-                      <p className="text-xl 2xl:text-3xl text-gray-900 font-light text-center mt-2">
-                        You are in! We will send you an email in a few days once
-                        the airdrop is ready.
-                      </p>
-                    </div>
-                  ) : (
-                    <div>
-                      <p className="text-2xl 2xl:text-4xl text-center mt-8">
-                        Oops...
-                      </p>
-                      <p className="text-xl 2xl:text-3xl text-gray-900 font-light text-center">
-                        There was an error... Please try again.
-                      </p>
-                    </div>
-                  )}
-                </>
-              ) : (
-                <>
-                  <div className="flex flex-row items-center mx-auto mt-6 xl:mt-10 2xl:mt-14">
-                    <div className="rounded-full w-8 xl:w-12 2xl:w-16 h-8 xl:h-12 2xl:h-16 border-2 border-gray-800 flex justify-center items-center">
-                      <p className="text-lg xl:text-xl 2xl:text-2xl font-semibold">
-                        1
-                      </p>
-                    </div>
-                    <img
-                      src={`/images/${
-                        step === Step.Wallet
-                          ? "arrowGray.svg"
-                          : "arrowBlack.svg"
-                      }`}
-                      className="mx-2"
-                    />
-                    <div
-                      className={`rounded-full w-8 xl:w-12 2xl:w-16 h-8 xl:h-12 2xl:h-16 border-2 flex justify-center items-center ${
-                        step === Step.Wallet
-                          ? "border-gray-500"
-                          : "border-black"
-                      }`}
-                    >
-                      <p
-                        className={`text-lg xl:text-xl 2xl:text-2xl font-semibold ${
-                          step === Step.Wallet ? "text-gray-500" : "text-black"
+          <h2 className="mx-auto text-center mt-8 w-9/12 text-2xl md:hidden">This site is not available on mobile...</h2>
+          <div className="hidden md:flex flex-col">
+            <p className="text-lg xl:text-xl 2xl:text-3xl mt-8 2xl:mt-12 text-gray-900 font-light text-center">
+              This airdrop sends 100 $POP to your wallet and 100 $POP to the
+              charities you select. Your airdropped tokens are locked until $POP
+              staking 2022). Simply verify BAYC charity allocations, and await
+              your airdrop. In meantime, join us in{" "}
+              <a
+                className="font-normal cursor-pointer"
+                href="https://discord.gg/RN4VGqPDwX"
+                target="_blank"
+              >
+                Discord
+              </a>{" "}
+              and follow us on{" "}
+              <a
+                className="font-normal cursor-pointer"
+                href="https://twitter.com/popcorn_DAO"
+                target="_blank"
+              >
+                Twitter
+              </a>
+            </p>
+            {PHASE === Phase.SignUp ? (
+              <>
+                {step === Step.End ? (
+                  <>
+                    {submitSuccess ? (
+                      <div>
+                        <p className="text-2xl 2xl:text-4xl text-center mt-8">
+                          Congratulations!
+                        </p>
+                        <p className="text-xl 2xl:text-3xl text-gray-900 font-light text-center mt-2">
+                          You are in! We will send you an email in a few days
+                          once the airdrop is ready.
+                        </p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p className="text-2xl 2xl:text-4xl text-center mt-8">
+                          Oops...
+                        </p>
+                        <p className="text-xl 2xl:text-3xl text-gray-900 font-light text-center">
+                          There was an error... Please try again.
+                        </p>
+                      </div>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <div className="flex flex-row items-center mx-auto mt-6 xl:mt-10 2xl:mt-14">
+                      <div className="rounded-full w-8 xl:w-12 2xl:w-16 h-8 xl:h-12 2xl:h-16 border-2 border-gray-800 flex justify-center items-center">
+                        <p className="text-lg xl:text-xl 2xl:text-2xl font-semibold">
+                          1
+                        </p>
+                      </div>
+                      <img
+                        src={`/images/${
+                          step === Step.Wallet
+                            ? "arrowGray.svg"
+                            : "arrowBlack.svg"
+                        }`}
+                        className="mx-2"
+                      />
+                      <div
+                        className={`rounded-full w-8 xl:w-12 2xl:w-16 h-8 xl:h-12 2xl:h-16 border-2 flex justify-center items-center ${
+                          step === Step.Wallet
+                            ? "border-gray-500"
+                            : "border-black"
                         }`}
                       >
-                        2
-                      </p>
+                        <p
+                          className={`text-lg xl:text-xl 2xl:text-2xl font-semibold ${
+                            step === Step.Wallet
+                              ? "text-gray-500"
+                              : "text-black"
+                          }`}
+                        >
+                          2
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <p className="text-lg xl:text-xl 2xl:text-2xl font-semibold text-center mt-8 2xl:mt-12">
-                    {step === Step.Wallet
-                      ? "Step 1: Connect with Metamask"
-                      : "Step 2: Enter your Email for the airdrop notification"}
-                  </p>
-                  {step === Step.Wallet ? (
-                    <button
-                      className="w-48 2xl:w-60 py-2 xl:py-4 px-5 mt-5 2xl:mt-6 flex flex-row items-center justify-center rounded-xl mx-auto bg-blue-600 hover:bg-blue-700"
-                      onClick={() => {
-                        activate(connectors.Injected);
-                        setStep(Step.Email);
-                      }}
-                    >
-                      <p className="text-lg text-white font-semibold 2xl:text-2xl">
-                        Connect Wallet
-                      </p>
-                    </button>
-                  ) : (
-                    <>
-                      {account && (
-                        <>
-                          <div className="relative z-20 mx-auto mt-5 shadow-lg bg-white rounded-xl py-2 px-2 w-10/12 2xl:w-8/12 flex flex-row items-center justify-between">
-                            <input
-                              type="email"
-                              name="email"
-                              className="w-10/12 p-2 border-none text-base mx-2 text-gray-900"
-                              placeholder="Email"
-                              required
-                              value={email}
-                              onChange={(e) => {
-                                setEmail(e.target.value.toLowerCase());
-                                setEmailError(
-                                  !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-                                    e.target.value.toLowerCase()
-                                  )
-                                );
-                              }}
-                            />
-                            <div className="clear">
+                    <p className="text-lg xl:text-xl 2xl:text-2xl font-semibold text-center mt-8 2xl:mt-12">
+                      {step === Step.Wallet
+                        ? "Step 1: Connect with Metamask"
+                        : "Step 2: Enter your Email for the airdrop notification"}
+                    </p>
+                    {step === Step.Wallet ? (
+                      <button
+                        className="w-48 2xl:w-60 py-2 xl:py-4 px-5 mt-5 2xl:mt-6 flex flex-row items-center justify-center rounded-xl mx-auto bg-blue-600 hover:bg-blue-700"
+                        onClick={() => {
+                          activate(connectors.Injected);
+                          setStep(Step.Email);
+                        }}
+                      >
+                        <p className="text-lg text-white font-semibold 2xl:text-2xl">
+                          Connect Wallet
+                        </p>
+                      </button>
+                    ) : (
+                      <>
+                        {account && (
+                          <>
+                            <div className="relative z-20 mx-auto mt-5 shadow-lg bg-white rounded-xl py-2 px-2 w-10/12 2xl:w-8/12 flex flex-row items-center justify-between">
                               <input
-                                type="submit"
-                                value="Register"
-                                name="submit"
-                                className="font-medium text-base bg-blue-600 hover:bg-blue-500 text-white rounded-xl px-4 py-2 cursor-pointer disabled:bg-gray-500"
-                                disabled={emailError}
-                                onClick={() => {
-                                  if (email.length > 0 && !emailError) {
-                                    if (
-                                      registeredContacts.emails.includes(email)
-                                    ) {
-                                      router.push("/alreadyRegistered");
-                                    } else {
-                                      addApe();
-                                    }
-                                  } else {
-                                    setEmailError(true);
-                                  }
+                                type="email"
+                                name="email"
+                                className="w-10/12 p-2 border-none text-base mx-2 text-gray-900"
+                                placeholder="Email"
+                                required
+                                value={email}
+                                onChange={(e) => {
+                                  setEmail(e.target.value.toLowerCase());
+                                  setEmailError(
+                                    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                                      e.target.value.toLowerCase()
+                                    )
+                                  );
                                 }}
                               />
+                              <div className="clear">
+                                <input
+                                  type="submit"
+                                  value="Register"
+                                  name="submit"
+                                  className="font-medium text-base bg-blue-600 hover:bg-blue-500 text-white rounded-xl px-4 py-2 cursor-pointer disabled:bg-gray-500"
+                                  disabled={emailError}
+                                  onClick={() => {
+                                    if (email.length > 0 && !emailError) {
+                                      if (
+                                        registeredContacts.emails.includes(
+                                          email
+                                        )
+                                      ) {
+                                        router.push("/alreadyRegistered");
+                                      } else {
+                                        addApe();
+                                      }
+                                    } else {
+                                      setEmailError(true);
+                                    }
+                                  }}
+                                />
+                              </div>
                             </div>
-                          </div>
-                          {emailError && (
-                            <p className="text-red-500 w-10/12 2xl:w-8/12 mx-auto mt-2">
-                              Please enter a valid email address
-                            </p>
-                          )}
-                        </>
-                      )}
-                    </>
-                  )}
-                  <div
-                    className={`w-48 xl:w-64 2xl:w-72 mx-auto border-2 border-gray-800 rounded-lg z-20 py-3 2xl:py-4 ${
-                      emailError
-                        ? "mt-0 xl:mt-4 2xl:mt-16"
-                        : "mt-8 xl:mt-12 2xl:mt-24"
-                    }`}
-                  >
-                    <p className="text-xl xl:text-3xl 2xl:text-4xl font-semibold text-center">
-                      {availableSlots}/{MAX_SLOTS} Left
-                    </p>
-                  </div>
-                </>
-              )}
-            </>
-          ) : (
-            <>
-              <p className="text-xl font-semibold text-center mt-8">
-                Connect with Metamask to claim your Airdrop
-              </p>
-              <button
-                className="w-48 2xl:w-60 py-4 px-5 mt-5 2xl:mt-16 flex flex-row items-center justify-center rounded-xl mx-auto bg-blue-600 hover:bg-blue-700"
-                onClick={() => {
-                  activate(connectors.Injected);
-                }}
-              >
-                <p className="text-lg text-white font-semibold 2xl:text-2xl">
-                  Connect Wallet
+                            {emailError && (
+                              <p className="text-red-500 w-10/12 2xl:w-8/12 mx-auto mt-2">
+                                Please enter a valid email address
+                              </p>
+                            )}
+                          </>
+                        )}
+                      </>
+                    )}
+                    <div
+                      className={`w-48 xl:w-64 2xl:w-72 mx-auto border-2 border-gray-800 rounded-lg z-20 py-3 2xl:py-4 ${
+                        emailError
+                          ? "mt-0 xl:mt-4 2xl:mt-16"
+                          : "mt-8 xl:mt-12 2xl:mt-24"
+                      }`}
+                    >
+                      <p className="text-xl xl:text-3xl 2xl:text-4xl font-semibold text-center">
+                        {availableSlots}/{MAX_SLOTS} Left
+                      </p>
+                    </div>
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <p className="text-xl font-semibold text-center mt-8">
+                  Connect with Metamask to claim your Airdrop
                 </p>
-              </button>
-            </>
-          )}
+                <button
+                  className="w-48 2xl:w-60 py-4 px-5 mt-5 2xl:mt-16 flex flex-row items-center justify-center rounded-xl mx-auto bg-blue-600 hover:bg-blue-700"
+                  onClick={() => {
+                    activate(connectors.Injected);
+                  }}
+                >
+                  <p className="text-lg text-white font-semibold 2xl:text-2xl">
+                    Connect Wallet
+                  </p>
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </div>
       <img
